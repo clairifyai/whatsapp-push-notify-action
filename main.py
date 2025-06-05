5ac71f1b92447733f31c9e6a399aa5034a56e1c3
Mehul Patel <mehul@clairify.ai>
2025-06-05 16:37:19 -0700

    Changed the update message

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ auth_token = os.environ['auth_token']
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-                              body='Yay! Push event triggered in master branch',
+                              body='Yay! User guide updated. Change log at docs.clairify.ai',
                               from_='whatsapp:+14155238886',
                               to='whatsapp:'+os.environ['to_whatsapp_no']
                           )
